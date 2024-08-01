a1a7b22a9af5f2efc94002610103d1aa76cf9ad3
SurbhiKaushik18 <sliet.surbhisingh2000@gmail.com>
2024-08-01 23:36:04 +0530

    Restrauant Management System

---
+++ hotelmenu.py
@@ -0,0 +1,19 @@
+menu={
+    'pizza':40,
+    'pasta':50,
+    'Burger':60,
+    'Salad':70,
+    'Coffee':80,
+}
+print("Welcome to PYTHON Restrauant.")
+
+print("Pizza: Rs40\nPasta: Rs50\nBurger: Rs60\nSalad: Rs70\nCoffee: Rs80 ")
+
+order_total = 0
+
+item1=input("Enter the name of item you want to order = ")
+if item in menu:
+    order_total+=menu[item1]
+    print("Your item {item1} has ben added to your order")
+else:
+    print("Ordered item")
