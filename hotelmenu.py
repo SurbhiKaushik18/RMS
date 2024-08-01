a51313daf95d325264cfbe6d2ac86c3ce841a849
SurbhiKaushik18 <sliet.surbhisingh2000@gmail.com>
2024-08-02 00:00:06 +0530

    Restrauant Management System

--- hotelmenu.py
+++ hotelmenu.py
@@ -12,7 +12,7 @@ print("Pizza: Rs40\nPasta: Rs50\nBurger: Rs60\nSalad: Rs70\nCoffee: Rs80 ")
 order_total = 0
 
 item1=input("Enter the name of item you want to order = ")
-if item in menu:
+if item1 in menu:
     order_total+=menu[item1]
     print("Your item {item1} has ben added to your order")
 else:
@@ -20,6 +20,12 @@ else:
 
 another_order=input("Do you want to add another item? (Yes/No)")
 
-if another_order==Yes:
-    print("Enter the name of item you want to order = ")
-    
+if another_order=="Yes":
+    item2=input("Enter the name of second item = ")
+    if item2 in menu:
+        order_total+=menu[item2]
+        print(f"Item {item2} has been added to order")
+    else:
+        print(f"Ordered item {item2} is not available")
+print(f"The total amount of items to pay is {order_total}")
+
