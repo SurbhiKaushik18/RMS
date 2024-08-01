3aea206e98736a222570f5dc2c0a0feeed264f26
SurbhiKaushik18 <sliet.surbhisingh2000@gmail.com>
2024-08-01 23:50:00 +0530

    Restrauant Management System

--- hotelmenu.py
+++ hotelmenu.py
@@ -16,4 +16,10 @@ if item in menu:
     order_total+=menu[item1]
     print("Your item {item1} has ben added to your order")
 else:
-    print("Ordered item")
+    print("Ordered item {item1} is not available yet")
+
+another_order=input("Do you want to add another item? (Yes/No)")
+
+if another_order==Yes:
+    print("Enter the name of item you want to order = ")
+    
